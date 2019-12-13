ad68970757102dfcc7605e6432a536dfd1e56eee
Mariusz Lisowski <mariusz.lisowski@netart.pl>
2019-12-13 10:49:28 +0100

    fix

--- setup.sh
+++ setup.sh
@@ -6,5 +6,4 @@ vagrant up
 ansible-playbook setup-openvas.yml   -i inventories/hosts  -D
 
 echo "------------- END Install ------------------"
-
 # ansible-playbook setup-openvas.yml   -i inventories/hosts  -D --tags "openvas-rebuild" -vvv
