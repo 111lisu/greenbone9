f501f672a5b521418b140fc7dedbf358584683ca
Mariusz Lisowski <mariusz.lisowski@netart.pl>
2019-12-13 13:13:55 +0100

    Add first commit pve lxc

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo "---------- START - ansible 2.7--------------"
 echo "------ Required VirtualBox, Vagrant --------" 
 vagrant up
-ansible-playbook setup-openvas.yml   -i inventories/hosts  -D
+ansible-playbook setup-openvas.yml -i inventories/hosts -D
 
 echo "------------- END Install ------------------"
 # ansible-playbook setup-openvas.yml   -i inventories/hosts  -D --tags "openvas-rebuild" -vvv
