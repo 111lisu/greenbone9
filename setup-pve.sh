aee704db02f2b189ed65a6c93ef200963bccb2f9
Mariusz Lisowski <mariusz.lisowski@netart.pl>
2019-12-16 07:04:48 +0100

    next test

--- setup-pve.sh
+++ setup-pve.sh
@@ -2,7 +2,7 @@
 
 echo "---------- START - ansible 2.7--------------"
 echo "-------------- PVE Server ------------------" 
-ansible-playbook setup-openvas-proxmox.yml -i inventories/hosts -D
+ansible-playbook setup-openvas-proxmox.yml -i inventories/hosts -D -vvv
 
 echo "------------- END Install ------------------"
 # ansible-playbook setup-openvas.yml   -i inventories/hosts  -D --tags "openvas-rebuild" -vvv
